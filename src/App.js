eb4185ca745b99ea7f7e94676a1d7bc6a5c2e4aa
Mahesh Jamdade <maheshmn121@gmail.com>
2020-05-18 08:24:55 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  /// TODO : STORE and fetch data to and from the cache
+   
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      newItem: "",
+    }
+  }
+  addItemToList(item) {
+    if (item !== "") {
+      var input = document.getElementById('todoBox');
+      console.log(input.value);
+      const newItem = {
+        value: item,
+        id: Date.now(),
+        isDone: false
+      };
+      const newList = [...this.state.list];
+      newList.push(newItem);
+      console.log(newList);
+      this.setState({
+        list: newList,
+        newItem: ""
+      });
+      input.value = '';
+    }
+  }
+  render() {
+    return (
+      <div className='App'>
+        <img className='App-logo' width="150" height="150" src={logo} />
+        <h2>Whats on Your Mind</h2>
+        <div className="row">
+          <SearchBar />
+          <button onClick={() => {
+            var input = document.getElementById("todoBox");
+            this.addItemToList(input.value);
+          }} className="ButtonStyle ">
+            Add to List
+          </button>
+        </div>
+        <div className='column'>
+          {this.state.list.map((item) => {
+            return <div className="listCard">
+              <p>{item.value}</p>
+            </div>
+          })
+          }
+        </div>
+      </div >);
+  }
 }
 
+// function AddButton() {
+//   return <button onClick={this.props.addItemToList} className="ButtonStyle ">
+//     Add to List
+//   </button>
+// }
+
+// function CheckBox() {
+//   return <input type="checkbox" />;
+// }
+function SearchBar() {
+  return (
+    <input type="text" id="todoBox" placeholder='Add a Quick todo...' className='SearchStyle'></input>
+  );
+}
 export default App;
